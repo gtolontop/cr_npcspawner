5884f7721f736d97edf5d2c7dd85a3b0984f221b
Cruck Store <cruckstore+github@gmail.com>
2025-06-25 20:00:36 +0200

    Add configuration for NPCs with coordinates, models, and dialogues

---
+++ config.lua
@@ -0,0 +1,30 @@
+Config = {}
+
+Config.DrawDistance = 50.0
+
+Config.NPCTable = {
+  {
+    coords = { x = 215.76, y = -810.12, z = 29.73, h = 160.0 },
+    model = "csb_mweather",
+    hasAnimation = true,
+    animDict = "amb@world_human_stand_impatient@male@no_sign@idle_a",
+    animName = "idle_a",
+    category = "vendor",
+    texts = {
+      "Achetez vos boissons ici !",
+      "Promos spéciales du jour !",
+      "Besoin d’un truc ? Je vends tout !"
+    }
+  },
+  {
+    coords = { x = 217.76, y = -810.12, z = 29.73, h = 160.0 },
+    model = "a_m_m_farmer_01",
+    hasAnimation = false,
+    category = "citizen",
+    texts = {
+      "Quel temps magnifique…",
+      "La vie est belle ici !",
+      "Salut voisin !"
+    }
+  }
+}
